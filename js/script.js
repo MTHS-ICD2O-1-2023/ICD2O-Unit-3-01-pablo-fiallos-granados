278a7ee3c58e273166d3c8f7db00739a22eca94b
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-03-28 18:48:14 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Pablo Fiallos
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function enterClicked() {
+  // input
+  const a = parseFloat(document.getElementById("aValue").value)
+  const b = parseFloat(document.getElementById("bValue").value)
+  const h = parseFloat(document.getElementById("hValue").value)
+  const area =  (a + b) / 2 * h
+  const areaRounded = area.toFixed(2)
+  // output
+  document.getElementById("user-info").innerHTML =
+    "A = " + areaRounded + "u <sup>2</sup>"
+}
